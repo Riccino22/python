d8b2e88c1be70a307f342366e56b34bc9016efa2
Riccino <riccardorinojosam@gmail.com>
2024-05-22 16:00:02 -0300

    "Added category field to contact us form and updated send_email function to include category in email subject"

--- web_company/contact_us.py
+++ web_company/contact_us.py
@@ -4,8 +4,9 @@ st.header("Contact Us")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
+    category = st.selectbox("Select a category", ["Social", "Professional", "Other"])
     message = st.text_area("Your message")
     btn_send = st.form_submit_button()
     if btn_send:
-        sem.send(user_email, message)
+        sem.send(user_email, category, message)
         
--- web_company/send_email.py
+++ web_company/send_email.py
@@ -2,20 +2,21 @@ import smtplib, ssl
 
 host = "smtp.gmail.com"
 port = 465
-email = "riccardoinojosa@gmail.com"
-password = "tehzyrklurhtbfue"
+email = "email@gmail.com"
+password = "password"
 
 context = ssl.create_default_context()
 
 default_content = f"""\
-Subject: sbj
+Subject: (About ctg) Message from: sbj
 
 bd
 """
 
-def send(user_email, message):
+def send(user_email, category, message):
     email_content = default_content
-    email_content = default_content.replace("sbj", "Msg from: " + user_email)
+    email_content = default_content.replace("sbj", user_email)
+    email_content = default_content.replace("ctg", category)
     email_content = email_content.replace("bd", message)
 
 
