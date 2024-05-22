9109c6bd6aba514200eaec954be9f635b7a99f6e
Riccino <riccardorinojosam@gmail.com>
2024-05-22 10:23:27 -0300

    "Added Gmail SMTP configuration and email sending code"

--- web_company/send_email.py
+++ web_company/send_email.py
@@ -1,2 +1,13 @@
 import smtplib, ssl
 
+host = "smtp.gmail.com"
+port = 465
+email = "riccardoinojosa@gmail.com"
+password = "tehzyrklurhtbfue"
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(email, password)
+    server.sendmail(email, email, "Hi bro")
+    
