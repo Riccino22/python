74d3635b27bcd2c586a30111838a9af33d701113
Riccardo Inojosa <68616811+Riccino22@users.noreply.github.com>
2024-05-26 10:31:52 -0300

    Removed unnecessary code

--- vocabulary_app/dictionary.py
+++ vocabulary_app/dictionary.py
@@ -21,23 +21,3 @@ def translate_word(word, source_lang='en', target_lang='es'):
     translation = translator.translate(word)
     return translation
 
-
-"""def translate_word(word):
-    translator = Translator()
-    translation = translator.translate(word, dest="es")
-    print(translation)
-    return translation.text
-
-translate_word("Friend")
-
-# Obtener la definición de una palabra
-word = "example"
-synsets = wordnet.synsets(word)
-
-# Imprimir la definición
-if synsets:
-    print(f"Definición de '{word}':")
-    for synset in synsets:
-        print(f"  - {synset.definition()}")
-else:
-    print(f"No se encontró la definición de '{word}'.")"""
